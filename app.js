3214283422352dd27d0b05abea95a645958b9f2f
pankajkuma.panigrahi <pankajkuma.panigrahi@servicenow.com>
2020-03-05 16:43:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import EventEmitter from 'events';
+import cors from "cors";
+
+let ee = new EventEmitter();
+ee.setMaxListeners(400);
+let messages = [];
+
+let app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.get('/messages',(req,res,next)=>{
+    res.status(200).send({
+        status:true,
+        response:messages
+    });
+});
+
+app.get('/message',(req,res,next)=>{
+    var addMessageListener = function(res){
+        ee.once('message', function(data){
+            res.status(200).send({
+                status:true,
+                response:data
+            });
+        })
+    }
+    addMessageListener(res)
+});
+
+
+app.post('/message', function(req, res){  
+    console.log("req.body:",req.body);
+    messages.push(req.body);
+    ee.emit('message', req.body)
+    res.status(200).send({
+        status:true
+    });
+})
+
+
+// app.delete('/messages',(req,res,next)=>{
+//     messages = [];
+//     res.status(200).send({
+//         status:true
+//     });
+// });
+
+app.listen(30006,()=>{
+    console.log(' ********** : running on 30006');
+})
+
+module.exports = app;
